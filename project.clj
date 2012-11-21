1bb124f04b29aa005b6093d1dc6d7d17f6409915
Kevin Downey <redchin@gmail.com>
2012-11-20 18:28:33 -0800

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.thelastcitadel/jenkins-clojure-injector "0.1.0-SNAPSHOT"
+(defproject org.thelastcitadel/jenkins-clojure-injector "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
